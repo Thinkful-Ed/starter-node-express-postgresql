00d3dab0c8f08c0123a700b0d948022d7fae8909
kchia <kchia87@gmail.com>
2020-12-01 10:40:13 -1000

    cascade deletes in migrations

--- src/db/migrations/20201130084720_createProductsCategoriesTable.js
+++ src/db/migrations/20201130084720_createProductsCategoriesTable.js
@@ -1,13 +1,18 @@
 exports.up = function (knex) {
   return knex.schema.createTable("products_categories", table => {
     table.integer("product_id").unsigned().notNullable();
-    table.foreign("product_id").references("product_id").inTable("products");
+    table
+      .foreign("product_id")
+      .references("product_id")
+      .inTable("products")
+      .onDelete("CASCADE");
 
     table.integer("category_id").unsigned().notNullable();
     table
       .foreign("category_id")
       .references("category_id")
-      .inTable("categories");
+      .inTable("categories")
+      .onDelete("CASCADE");
 
     table.timestamps(true, true);
   });
