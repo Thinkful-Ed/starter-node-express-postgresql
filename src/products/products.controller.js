b970b7be2321ac8b3193cdd80584f11c62b2bb7f
kchia <kchia87@gmail.com>
2020-11-30 20:40:30 -1000

    create CRUD functionality

--- src/products/products.controller.js
+++ src/products/products.controller.js
@@ -1,14 +1,30 @@
-function read(req, res, next) {
-  res.json({ data: { product_title: "some product title" } });
-}
+const ProductsService = require("./products.service");
 
-function list(req, res, next) {
-  res.json({
-    data: [{ product_title: "product 1" }, { product_title: "product 2" }],
+function productExists(req, res, next) {
+  const error = { status: 404, message: `Product cannot be found.` };
+
+  const { productId } = req.params;
+  if (!productId) return next(error);
+
+  ProductsService.getProductById(productId).then(product => {
+    if (!product) return next(error);
+    res.locals.product = product;
+    next();
   });
 }
 
+function read(req, res, next) {
+  const { product } = res.locals;
+  res.json({ data: product });
+}
+
+function list(req, res, next) {
+  ProductsService.getAllProducts().then(products =>
+    res.json({ data: products })
+  );
+}
+
 module.exports = {
-  read: [read],
+  read: [productExists, read],
   list: [list],
 };
