f154f66ae6a4baa650c4c32a13f330721151a107
kchia <kchia87@gmail.com>
2021-01-27 23:07:04 -1000

    update aggregate controllers

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -3,6 +3,18 @@ const controller = require("./products.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
-router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
+router
+  .route("/out_of_stock_count")
+  .get(controller.listOutOfStockCount)
+  .all(methodNotAllowed);
+router
+  .route("/price_summary")
+  .get(controller.listPriceSummary)
+  .all(methodNotAllowed);
+router
+  .route("/total_weight_by_product")
+  .get(controller.listTotalWeightByProduct)
+  .all(methodNotAllowed);
+router.route("/:productId").get(controller.read).all(methodNotAllowed);
 
 module.exports = router;
