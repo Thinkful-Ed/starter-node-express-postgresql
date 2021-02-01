0178e11d76da324bda1c2362cf91937c6323e310
kchia <kchia87@gmail.com>
2021-02-01 09:41:48 -1000

    use dashes instead of underscores in url path

--- src/products/products.router.js
+++ src/products/products.router.js
@@ -4,15 +4,15 @@ const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/").get(controller.list).all(methodNotAllowed);
 router
-  .route("/out_of_stock_count")
+  .route("/out-of-stock-count")
   .get(controller.listOutOfStockCount)
   .all(methodNotAllowed);
 router
-  .route("/price_summary")
+  .route("/price-summary")
   .get(controller.listPriceSummary)
   .all(methodNotAllowed);
 router
-  .route("/total_weight_by_product")
+  .route("/total-weight-by-product")
   .get(controller.listTotalWeightByProduct)
   .all(methodNotAllowed);
 router.route("/:productId([0-9]+)").get(controller.read).all(methodNotAllowed);
