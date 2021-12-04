82970dfe005f72307dfff99e3904635c4bf3b29e
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-04 11:13:48 -0800

    initialized knex config file

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: "",
+  },
+
+  // test: {
+  //   client: "postgresql",
+  //   pool: { min: 1, max: 5 },
+  //   connection: {
+  //     database: "db_test",
+  //     user: "username_test",
+  //     password: "password_test",
+  //   },
+  // },
+
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations",
+  //   },
+  // },
+
+  // production: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations",
+  //   },
+  // },
+};
