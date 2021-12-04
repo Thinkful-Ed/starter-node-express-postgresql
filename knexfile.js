0bcf73c74aed98b4a11e5b93f9be44ceebb7f72a
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-04 11:20:55 -0800

    Imported connection from EV

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,11 @@
 // Update with your config settings.
+const dotenv = require("dotenv").config();
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: "",
+    connection: DATABASE_URL,
   },
 
   // test: {
