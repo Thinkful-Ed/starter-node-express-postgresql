ba5687bc89df8b31e1a70cd07e883efe1871800c
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-04 17:18:07 -0800

    Added categories.service to handle knex queries for get

--- src/categories/categories.controller.js
+++ src/categories/categories.controller.js
@@ -1,13 +1,12 @@
-async function list(req, res, next) {
-  res.json({
-    data: [
-      { category_name: "category 1" },
-      { category_name: "category 2" },
-      { category_name: "category 3" },
-    ],
-  });
-}
+const categoriesService = require("./categories.service");
+
+const list = (req, res, next) => {
+  categoriesService
+    .list()
+    .then((data) => res.json({ data }))
+    .catch(next);
+};
 
 module.exports = {
-  list: [list],
+  list,
 };
