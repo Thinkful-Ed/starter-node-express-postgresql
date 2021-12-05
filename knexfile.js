07a0e9cced2a73f1630d8b9a9c5fa85045b6f403
Joseph Thomas Vasquez <joetv.tech@gmail.com>
2021-12-04 16:50:27 -0800

    Added seed config to knex and created seeds with seed data for all tables

--- knexfile.js
+++ knexfile.js
@@ -10,6 +10,9 @@ module.exports = {
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
   // test: {
