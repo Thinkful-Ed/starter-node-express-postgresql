1f71695d8a8e14749552a17e5ad1302f92022f89
Thomas Sheehan <tsheehan@indeed.com>
2024-08-22 10:36:49 +0900

    knex project configuration setup

---
+++ knexfile.js
@@ -0,0 +1,11 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: "",
+  },
+};
